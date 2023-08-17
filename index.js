e82224bba28e3f2ff70bb4bf7d090733b6ccf275
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-08-17 15:48:07 +0200

    add the player and scores to index.js

--- index.js
+++ index.js
@@ -1,2 +1,72 @@
-const player = ["player1", "player2"];
+const player = [
+  {
+    player: "Anna",
+    score: "04",
+  },
+  {
+    player: "Berna",
+    score: "05",
+  },
+  {
+    player: "Bonnie",
+    score: "02",
+  },
+  {
+    player: "David K.",
+    score: "02",
+  },
+  {
+    player: "David v.H.",
+    score: "01",
+  },
+  {
+    player: "Elli",
+    score: "07",
+  },
+  {
+    player: "Henrique",
+    score: "02",
+  },
+  {
+    player: "Jaoh",
+    score: "02",
+  },
+  {
+    player: "Leo",
+    score: "02",
+  },
+  {
+    player: "Marcus",
+    score: "02",
+  },
+  {
+    player: "Miriam",
+    score: "01",
+  },
+  {
+    player: "Pia",
+    score: "03",
+  },
+  {
+    player: "Ping",
+    score: "02",
+  },
+  {
+    player: "Shwan",
+    score: "02",
+  },
+  {
+    player: "Stephan",
+    score: "03",
+  },
+  {
+    player: "Tien",
+    score: "02",
+  },
+  {
+    player: "Tobi",
+    score: "05",
+  },
+];
+
 console.log(player);
