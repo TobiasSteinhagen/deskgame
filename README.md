0c4a2d4bf05b301bfda1851d53e4a981333ab06f
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-08-17 15:30:42 +0200

    add deskgame to github

---
+++ README.md
@@ -0,0 +1,10 @@
+Every 🐸 sent a picture of his/her desk to coach Jess.
+
+We will guess, on a regular basis, after the coach posted a random desk picture whose desk this is.
+
+1. If you have a guess, give a ✅ in zoom
+2. Write the name of the person in the Slack thread, regarding this picture
+
+   ------- DON'T SEND IT YET 🤓---------
+
+3. Coach will give us a countdwon and after the countdown, hit the hell out of the send button in Slack and let the fun begin 🔥
---
+++ index.js
@@ -0,0 +1,70 @@
+// const player = [
+//   {
+//     name: "Anna",
+//     points: "04",
+//   },
+//   {
+//     name: "Berna",
+//     points: "05",
+//   },
+//   {
+//     name: "Bonnie",
+//     points: "02",
+//   },
+//   {
+//     name: "David K.",
+//     points: "02",
+//   },
+//   {
+//     name: "David v.H.",
+//     points: "01",
+//   },
+//   {
+//     name: "Elli",
+//     points: "07",
+//   },
+//   {
+//     name: "Henrique",
+//     points: "02",
+//   },
+//   {
+//     name: "Jaoh",
+//     points: "02",
+//   },
+//   {
+//     name: "Leo",
+//     points: "02",
+//   },
+//   {
+//     name: "Marcus",
+//     points: "02",
+//   },
+//   {
+//     name: "Miriam",
+//     points: "01",
+//   },
+//   {
+//     name: "Pia",
+//     points: "03",
+//   },
+//   {
+//     name: "Ping",
+//     points: "02",
+//   },
+//   {
+//     name: "Shwan",
+//     points: "02",
+//   },
+//   {
+//     name: "Stephan",
+//     points: "03",
+//   },
+//   {
+//     name: "Tien",
+//     points: "02",
+//   },
+//   {
+//     name: "Tobi",
+//     points: "05",
+//   },
+// ];
